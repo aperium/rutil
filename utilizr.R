185de891904b050a470ff4f9242ca4cedcea6f1e
Daniel R Williams <aperium@gmail.com>
2023-10-10 14:12:20 -0400

    Added first function "bestmatch"

---
+++ utilizr.R
@@ -0,0 +1,16 @@
+#!R 
+
+
+
+
+
+
+
+# function for summarizing names by finding the one the best fits a match in another list
+# select is a list/vector of strings to select from.
+# against is a list/vector of strings to compare against
+librarian::shelf(tibble, foreach, doParallel, stringdist)
+bestmatch <- function(select, against, ...) {
+  tibble(var = select, mindist= foreach(i=1:length(var)) %dopar% {min(afind(against, var, ...)$distance[,i])} %>% unlist()) %>% filter(mindist == min(.$mindist)) %>% .$var
+}
+
