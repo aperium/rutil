08b6fe06017e40bb59fe0d3b8c4b8b428a5352ac
Daniel R. Williams <aperium@gmail.com>
2024-02-14 14:11:31 -0500

    trying to debug warndings with undoced args

--- R/google_get_address.R
+++ R/google_get_address.R
@@ -9,17 +9,17 @@
 
 #' quick using google to get the address from any text query. Works best if the query is a mostly complete address.
 #' The plural version lets query be a list of strings. Internally reduce to unique queries and then re expand to same lenth as query for return.
-google_get_address <- function(query) {
-  query <- query |> stringr::str_replace_all("[:space:]","*")
+google_get_address <- function(query = NULL) {
+  q <- query |> stringr::str_replace_all("[:space:]","*")
   b <- chromote::ChromoteSession$new()
-  b$Page$navigate(paste0("https://www.google.com/search?q=",query,"*va&sclient=gws-wiz-serp"))
+  b$Page$navigate(paste0("https://www.google.com/search?q=",q,"*va&sclient=gws-wiz-serp"))
   b$Page$loadEventFired()
   x <- b$DOM$getDocument() %>% { b$DOM$querySelector(.$root$nodeId, ".LrzXr") } %>% { b$DOM$getOuterHTML(.$nodeId) } |> unlist() |> stringr::str_trim() |> stringr::str_remove_all("(<.*>(?=[^$]))|((?<=[^^])<.*>)")
   b$close()
   return(x)
 }
 
-google_get_addresses <- function(query) {
+google_get_addresses <- function(query = NULL) {
   query <- query |> stringr::str_replace_all("[:space:]","*")
   uniquery <- query |> stringr::str_unique()
   tmp_join <- function(...) dplyr::full_join(..., by = dplyr::join_by(.data$query, .data$result))
