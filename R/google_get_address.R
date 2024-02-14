6bcb87c1ac8d2923be89fd52aab65f239a90e04e
Daniel R. Williams <aperium@gmail.com>
2024-02-14 10:58:34 -0500

    added google_get_addres function

---
+++ R/google_get_address.R
@@ -0,0 +1,38 @@
+#' Google Get Address
+#'
+#'  Author: Daniel R. Williams
+#'  Date: 14 Feb 2024
+#'
+#' cran_packs <- c("rvest","stringr","chromote", "dplyr", "foreach", "tibble")
+
+
+
+#' quick using google to get the address from any text query. Works best if the query is a mostly complete address.
+#' The plural version lets query be a list of strings. Internally reduce to unique queries and then re expand to same lenth as query for return.
+google_get_address <- function(query) {
+  query <- query |> stringr::str_replace_all("[:space:]","*")
+  b <- chromote::ChromoteSession$new()
+  b$Page$navigate(paste0("https://www.google.com/search?q=",query,"*va&sclient=gws-wiz-serp"))
+  b$Page$loadEventFired()
+  x <- b$DOM$getDocument() |> { b$DOM$querySelector(.$root$nodeId, ".LrzXr") } |> { b$DOM$getOuterHTML(.$nodeId) } |> unlist() |> stringr::str_trim() |> stringr::str_remove_all("(<.*>(?=[^$]))|((?<=[^^])<.*>)")
+  b$close()
+  return(x)
+}
+
+google_get_addresses <- function(query) {
+  query <- query |> stringr::str_replace_all("[:space:]","*")
+  uniquery <- query |> stringr::str_unique()
+  tmp_join <- function(...) dplyr::full_join(..., by = dplyr::join_by(query, result))
+  b <- chromote::ChromoteSession$new()
+  results <- foreach::foreach(i=1:length(uniquery), .combine = tmp_join, .multicombine = FALSE) %do% {
+    b$Page$navigate(paste0("https://www.google.com/search?q=",uniquery[i],"*va&sclient=gws-wiz-serp"))
+    b$Page$loadEventFired()
+    x<-NA
+    try(x <- b$DOM$getDocument() |> { b$DOM$querySelector(.$root$nodeId, ".LrzXr") } |> { b$DOM$getOuterHTML(.$nodeId) } |> unlist() |> stringr::str_trim() |> stringr::str_remove_all("(<.*>(?=[^$]))|((?<=[^^])<.*>)"), silent = TRUE)
+    tibble::tibble_row(query=uniquery[i],result=x)
+  } |>
+    dplyr::right_join(query |> tibble::as_tibble_col(column_name = "query"), by = dplyr::join_by(query)) |>
+    pull::pull(result)
+  b$close()
+  return(results)
+}
