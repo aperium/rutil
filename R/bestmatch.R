1a3b9b238dd559101da371e41c2b16302a5d753d
Daniel R. Williams <aperium@gmail.com>
2024-02-14 13:14:41 -0500

    best match commented out. Needs replaced

--- R/bestmatch.R
+++ R/bestmatch.R
@@ -3,7 +3,7 @@
 #' select is a list or vector of strings to select from.
 #' against is a list or vector of strings to compare against
 ## librarian::shelf(tibble, foreach, doParallel, stringdist)
-bestmatch <- function(select, against, ...) {
-  tibble(var = select, mindist= foreach(i=1:length(var)) %dopar% {min(afind(against, var, ...)$distance[,i])} %>% unlist()) %>% filter(mindist == min(.$mindist)) %>% .$var
-}
+# bestmatch <- function(select, against, ...) {
+#   tibble(var = select, mindist= foreach(i=1:length(var)) %dopar% {min(stringdist::afind(against, var, ...)$distance[,i])} %>% unlist()) %>% filter(mindist == min(.$mindist)) %>% .$var
+# }
 
