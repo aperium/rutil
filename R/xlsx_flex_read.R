067ebe5c26b716d8ed0b906a5153e290ef41f158
Daniel R. Williams <danielw@greenstreetgrowers.com>
2024-02-26 07:42:20 -0500

    commented out flexread. too many dependency issues rn

--- R/xlsx_flex_read.R
+++ R/xlsx_flex_read.R
@@ -25,30 +25,30 @@
 #'
 
 
-xlsx_flex_read <- function(path, sheet = NULL, range = NULL, col_names = TRUE, ...) {
-  if(nzchar(find.package("readxl"))) use_which <- "readxl"
-  else if (nzchar(find.package("openxlsx"))) use_which <- "openxlsx"
-  else if (nzchar(find.package("xlsx"))) use_which <- "xlsx"
-  else return("please install one of: readxl, openxlsx, xlsx")
-  #
-  sheet_index <- switch (typeof(sheet),
-                         "NULL" = 1,
-                         "double" = sheet,
-                         "integer" = sheet,
-                         "numeric" = sheet,
-                         "character" = NULL
-  )
-  sheet_name <- switch (typeof(sheet),
-                        "NULL" = NULL,
-                        "double" = NULL,
-                        "integer" = NULL,
-                        "numeric" = NULL,
-                        "character" = sheet
-  )
-  # use_which <- "readxl"
-  switch (use_which,
-          "readxl" = readxl::read_xlsx(path = path, sheet = sheet, range = range, col_names = col_names, guess_max = Inf, ...),
-          "openxlsx" = openxlsx::read.xlsx(xlsxFile = path, sheet = ifelse(!rlang::is_null(sheet_index), sheet_index, 1) , colNames = col_names, check.names = FALSE, sep.names = " ", ...),
-          "xlsx" = xlsx::read.xlsx(file = path, sheetIndex = sheet_index, sheetName = sheet_name, header = col_names, as.data.frame=TRUE, check.names = FALSE, ...)
-  )
-}
+# xlsx_flex_read <- function(path, sheet = NULL, range = NULL, col_names = TRUE, ...) {
+#   if(nzchar(find.package("readxl"))) use_which <- "readxl"
+#   else if (nzchar(find.package("openxlsx"))) use_which <- "openxlsx"
+#   else if (nzchar(find.package("xlsx"))) use_which <- "xlsx"
+#   else return("please install one of: readxl, openxlsx, xlsx")
+#   #
+#   sheet_index <- switch (typeof(sheet),
+#                          "NULL" = 1,
+#                          "double" = sheet,
+#                          "integer" = sheet,
+#                          "numeric" = sheet,
+#                          "character" = NULL
+#   )
+#   sheet_name <- switch (typeof(sheet),
+#                         "NULL" = NULL,
+#                         "double" = NULL,
+#                         "integer" = NULL,
+#                         "numeric" = NULL,
+#                         "character" = sheet
+#   )
+#   # use_which <- "readxl"
+#   switch (use_which,
+#           "readxl" = readxl::read_xlsx(path = path, sheet = sheet, range = range, col_names = col_names, guess_max = Inf, ...),
+#           "openxlsx" = openxlsx::read.xlsx(xlsxFile = path, sheet = ifelse(!rlang::is_null(sheet_index), sheet_index, 1) , colNames = col_names, check.names = FALSE, sep.names = " ", ...),
+#           "xlsx" = xlsx::read.xlsx(file = path, sheetIndex = sheet_index, sheetName = sheet_name, header = col_names, as.data.frame=TRUE, check.names = FALSE, ...)
+#   )
+# }
